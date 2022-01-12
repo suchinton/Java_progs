0b9e30c1bd83183a9568aa51cf9eabd927831120
suchinton <suchinton.chakravarty@s.amity.com>
2022-01-12 14:25:51 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   student.class
    	modified:   student.java
    
     Changes not staged for commit:
    	deleted:    ../test/info.class
    	deleted:    ../test/info.java

--- 2_prog/student.java
+++ 2_prog/student.java
@@ -12,7 +12,7 @@ class student
         System.out.println("| Java prog to display 5 entries|");
         System.out.println("|-------------------------------|");
         
-        System.out.println("Name      :: email                    :: 123 ");
+        System.out.println("Name      :: email                    :: id \n");
         System.out.println("Suchinton :: Suchinton.2001@gmail.com :: 234 ");
         System.out.println("Megha     :: Megha.chak@gmail.com     :: 112 ");
         System.out.println("Lucky     :: Lucky_man@yahoo.com      :: 122 ");
