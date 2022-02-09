5d47a9ee6fdc0421f71fba32443a4b9cb5ed3e3c
suchinton <suchinton.chakravarty@s.amity.edu>
2022-02-09 12:59:48 +0530

    modified:   6_Quad_eq/quad_eq.class 	modified:   6_Quad_eq/quad_eq.java 	.vscode/

--- 6_Quad_eq/quad_eq.java
+++ 6_Quad_eq/quad_eq.java
@@ -7,13 +7,13 @@ public class quad_eq
 
     public static void main(String[] args) 
     {
-        System.out.print("\033[H\033[2J");
+        System.out.print("\033[H\033[2J"); 
         System.out.flush();
         System.out.println("| Suchinton (A2345920063)                               |");
         System.out.println("|-------------------------------------------------------|");
         System.out.println("| Java prog to solve quad eqn using if, else if and else|");
         System.out.println("|-------------------------------------------------------|");
-
+               
         int a, b, c;
         double desc ;
         double root1, root2 ;
