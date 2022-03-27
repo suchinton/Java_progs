d6eec6dae91b5056ab1894a99af5649f9db22736
suchinton <suchinton.chakravarty@s.amity.edu>
2022-03-27 14:26:51 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   11_file_hand/a1.txt
    	modified:   11_file_hand/a2.txt
    	modified:   11_file_hand/a3.txt
    	modified:   11_file_hand/file_read_write_mult_f.class
    	modified:   11_file_hand/file_read_write_mult_f.java
    	new file:   11_file_hand/tempCodeRunnerFile.java

--- 11_file_hand/file_read_write_mult_f.java
+++ 11_file_hand/file_read_write_mult_f.java
@@ -13,22 +13,21 @@ public class file_read_write_mult_f
         FileOutputStream b2 = new FileOutputStream("a2.txt");
         FileOutputStream b3 = new FileOutputStream("a3.txt");
         
-        ByteArrayOutputStream f1 = new ByteArrayOutputStream();   
+        ByteArrayOutputStream f1 = new ByteArrayOutputStream();
 
-        //String data ="suchinton";
-        System.out.println("enter data to be entered into File 1 : ");
+        System.out.print("enter data to be entered into File 1 : ");
         String data = input.nextLine();
 
         byte buf[] = data.getBytes();
 
         b1.write(buf);
         
-        FileReader b4 = new FileReader("a1.txt");
+        FileReader fr = new FileReader("a1.txt");
         
         int ch;
         System.out.println("\nCopying from File 1");
         
-        while((ch = b4.read())!=-1)
+        while((ch = fr.read())!=-1)
         {
             f1.write(ch);
         }
@@ -37,32 +36,35 @@ public class file_read_write_mult_f
         f1.writeTo(b3);
 
         System.out.println("\nwritten to File 2 and 3");
-        
+
         System.out.println("\n----------------------------------------------");
         System.out.println("\nContents of File. 1");
-        b4 = new FileReader("a1.txt");
-        while((ch = b4.read())!=-1)
+        fr = new FileReader("a1.txt");
+        while((ch = fr.read())!=-1)
         {
             System.out.print((char) ch);
         }
         System.out.println("\n----------------------------------------------");
 
         System.out.println("Contents of File. 2");
-        b4 = new FileReader("a2.txt");
-        while((ch = b4.read())!=-1)
+        fr = new FileReader("a2.txt");
+        while((ch = fr.read())!=-1)
         {
             System.out.print((char) ch);
         }
         System.out.println("\n----------------------------------------------");
 
         System.out.println("Contents of File. 3");
-        b4 = new FileReader("a3.txt");
-        while((ch = b4.read())!=-1)
+        fr = new FileReader("a3.txt");
+        while((ch = fr.read())!=-1)
         {
             System.out.print((char) ch);
         }
         System.out.println("\n----------------------------------------------");
 
         f1.flush();
+        b1.close();
+        b2.close();
+        b3.close();
     }
 }
